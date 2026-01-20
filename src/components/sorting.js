60ec718728be4a81d4fe1d6cbadd3c1c0d7247f0
kitnorm <andrey.kitnormal@gmail.com>
2026-01-20 04:16:58 +0100

    release: final version

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,30 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import { sortCollection, sortMap } from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
-        let field = null;
-        let order = null;
+  return (data, state, action) => {
+    let field = null;
+    let order = null;
 
-        if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
+    if (action && action.name === "sort") {
+      // @todo: #3.1 — запомнить выбранный режим сортировки
+      action.dataset.value = sortMap[action.dataset.value];
+      field = action.dataset.field;
+      order = action.dataset.value;
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
-        } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+      // @todo: #3.2 — сбросить сортировки остальных колонок
+      columns.forEach((column) => {
+        // Перебираем элементы (в columns у нас массив кнопок)
+        if (column.dataset.field !== action.dataset.field) {
+          // Если это не та кнопка, что нажал пользователь
+          column.dataset.value = "none"; // тогда сбрасываем её в начальное состояние
         }
+      });
 
-        return sortCollection(data, field, order);
+      // @todo: #3.2 — сбросить сортировки остальных колонок
+    } else {
+      // @todo: #3.3 — получить выбранный режим сортировки
     }
+
+    return sortCollection(data, field, order);
+  };
 }
